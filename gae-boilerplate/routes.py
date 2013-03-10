c1d05904d9672a452a6a3c7f7123bc5c53e6312e
alexsmx <alexsmx@gmail.com>
2013-03-10 14:59:19 -0600

    first commit

---
+++ gae-boilerplate/routes.py
@@ -0,0 +1,25 @@
+"""
+Using redirect route instead of simple routes since it supports strict_slash
+Simple route: http://webapp-improved.appspot.com/guide/routing.html#simple-routes
+RedirectRoute: http://webapp-improved.appspot.com/api/webapp2_extras/routes.html#webapp2_extras.routes.RedirectRoute
+"""
+
+from webapp2_extras.routes import RedirectRoute
+from web import handlers
+from web import angellist
+secure_scheme = 'https'
+
+_routes = [
+    RedirectRoute('/secure/', handlers.SecureRequestHandler, name='secure', strict_slash=True),
+    RedirectRoute('/admin/home/', handlers.adminHomeHandler, name='admin-home', strict_slash=True),
+    RedirectRoute('/admin/search/', handlers.adminSearchHandler, name='admin-search', strict_slash=True),
+]
+
+def get_routes():
+    return _routes
+
+def add_routes(app):
+    if app.debug:
+        secure_scheme = 'http'
+    for r in _routes:
+        app.router.add(r)
